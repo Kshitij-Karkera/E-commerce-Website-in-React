2472640245ff896bf530e15eeb65b6b9c68e828c
Kshitij-Karkera <kshitu2308@gmail.com>
2026-05-10 12:18:31 -0500

    Updated SearchCategory.js to work with the Loader Component

--- src/pages/SearchCategory.js
+++ src/pages/SearchCategory.js
@@ -1,13 +1,12 @@
 import { useStateValue } from '../StateProvider';
 import SearchDetails from '../components/SearchDetails/SearchDetails';
-
+import EpicLoader from '../components/EpicLoader/EpicLoader';
 
 function SearchCategory() {
   const [{ productDetails }] = useStateValue();
 
-  // Display loading state if productDetails is not available
   if (!productDetails || productDetails.length === 0) {
-    return <div>Loading..</div>;
+    return <EpicLoader label="Fetching results" />;
   }
 
   return <SearchDetails />;
